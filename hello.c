fa6c99586a0258ccb193fdddfebb61077dbb2204
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-01-21 18:37:53 -0800

    update step 1 formatting to adhere to guidelines

--- hello.c
+++ hello.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+
 int main()
 {
+int MULTIPLE = 5;
+int product;
 int x = 12;
 float y = 3.14;
-char s[] = "Hello, World";
-printf("x is %d", x);
-printf("y is %.2f", y);
-printf("%s",s);
-for (int i = 0; i<4; i++)
+char s[] = "Hello, world!";
+printf("x is %d, ", x);
+printf("y is %f\n", y);
+printf("%s\n",s);
+for (int i = 0; i<=4; i++)
 {
-printf("test");
-
-
+product = i * MULTIPLE;
+printf("%d",i);
+printf(" x ");
+printf("%d",MULTIPLE);
+printf(" = %d\n",product);
 }
 
-
-return 0;
 }
