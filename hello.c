d5094456f2054bd55881f53e312930196f5ab42e
Daniel Lounsbury <lounsbud@oregonstate.edu>
2024-01-21 17:50:37 -0800

    inital commit/ finish steps 1 and 2

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int x = 12;
+float y = 3.14;
+char s[] = "Hello, World";
+printf("x is %d", x);
+printf("y is %.2f", y);
+printf("%s",s);
+for (int i = 0; i<4; i++)
+{
+printf("test");
+
+
+}
+
+
+return 0;
+}
